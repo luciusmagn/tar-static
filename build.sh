66f9838252b01b396e5e490c8dc2ff1923b52950
Lukáš Hozda <luk.hozda@gmail.com>
2017-11-18 20:06:43 +0100

    sed initial commit

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# build static gawk because we need exercises in minimalism
+# build static sed because we need exercises in minimalism
 # MIT licensed: google it or see robxu9.mit-license.org.
 #
 # For Linux, also builds musl for truly static linking.
 
-gawk_version="4.2.0"
+sed_version="4.4"
 musl_version="1.1.15"
 
 platform=$(uname -s)
@@ -19,11 +19,11 @@ mkdir build # make build directory
 pushd build
 
 # download tarballs
-echo "= downloading gawk"
-curl -LO http://ftp.gnu.org/gnu/gawk/gawk-${gawk_version}.tar.xz
+echo "= downloading sed"
+curl -LO http://ftp.gnu.org/gnu/sed/sed-${sed_version}.tar.xz
 
-echo "= extracting gawk"
-tar xJf gawk-${gawk_version}.tar.xz
+echo "= extracting sed"
+tar xJf sed-${sed_version}.tar.xz
 
 if [ "$platform" = "Linux" ]; then
   echo "= downloading musl"
@@ -50,12 +50,12 @@ else
   echo "= (This is mainly due to non-static libc availability.)"
 fi
 
-echo "= building gawk"
+echo "= building sed"
 
-pushd gawk-${gawk_version}
+pushd sed-${sed_version}
 env FORCE_UNSAFE_CONFIGURE=1 CFLAGS="$CFLAGS -Os -ffunction-sections -fdata-sections" LDFLAGS='-Wl,--gc-sections' ./configure
 make
-popd # gawk-${gawk_version}
+popd # sed-${sed_version}
 
 popd # build
 
@@ -64,9 +64,9 @@ if [ ! -d releases ]; then
 fi
 
 echo "= striptease"
-strip -s -R .comment -R .gnu.version --strip-unneeded build/gawk-${gawk_version}/gawk
+strip -s -R .comment -R .gnu.version --strip-unneeded build/sed-${sed_version}/sed/sed
 echo "= compressing"
-upx --ultra-brute build/gawk-${gawk_version}/gawk
-echo "= extracting gawk binary"
-cp build/gawk-${gawk_version}/gawk releases
+upx --ultra-brute build/sed-${sed_version}/sed/sed
+echo "= extracting sed binary"
+cp build/sed-${sed_version}/sed/sed releases
 echo "= done"
